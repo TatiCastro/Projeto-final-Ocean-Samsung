6bf420b2ddcf0ff0e1f486bf5dbb518ef7e0c03b
TatiCastro <88949545+TatiCastro@users.noreply.github.com>
2022-05-29 23:14:09 -0300

    Add files via upload

---
+++ ProjetoElevador/Model/Elevador.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoElevador.Model
+{
+    internal class Elevador
+    {
+        
+        private int andarAtual { get; set; } 
+        private int totalAndares { get; set; } 
+        private int capacidadeElevador { get; set; } 
+        private int qtdPessoas { get; set; } 
+
+        public Elevador() 
+        {
+            andarAtual = 0; // inicia no térreo 
+            qtdPessoas = 0; //começa sem pessoas  
+        }
+
+        public void Inicio() 
+        {
+            
+            Console.WriteLine("Capacidade do elevador: 6 pessoas");
+            
+            Console.WriteLine("Total de andares : 10 andares");
+           
+        }
+
+        public void Entrar() 
+        {
+            if (qtdPessoas < capacidadeElevador)
+            {
+                qtdPessoas++;
+                Console.WriteLine("Andar: {0}, Lotação: {1} passageiro(s)", andarAtual, qtdPessoas);
+            }
+            else
+            {
+                Console.WriteLine("ERRO: Lotado! Por favor aguarde.");
+            }
+        }
+
+        public void Sair() 
+        {
+            if (qtdPessoas > 0)
+            {
+                qtdPessoas--;
+                Console.WriteLine("Andar: {0}, Lotação: {1} passageiro(s)", andarAtual, qtdPessoas);
+            }
+            else
+            {
+                Console.WriteLine("ERRO: Elevador vazio!");
+            }
+        }
+
+        public void Subir() 
+        {
+            if (andarAtual < totalAndares)
+            {
+                andarAtual++;
+                Console.WriteLine("Andar: {0}, Lotação: {1} passageiro(s)", andarAtual, qtdPessoas);
+            }
+            else
+            {
+                Console.WriteLine("ERRO: Elevador no último andar!");
+            }
+        }
+
+        public void Descer() 
+        {
+            if (andarAtual > 0)
+            {
+                andarAtual--;
+                Console.WriteLine("Andar: {0}, Lotação: {1} passageiro(s)", andarAtual, qtdPessoas);
+            }
+            else
+            {
+                Console.WriteLine("ERRO: Elevador no térreo!");
+            }
+        }
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ ProjetoElevador/Program.cs
@@ -0,0 +1,58 @@
+﻿using ProjetoElevador.Model;
+using System;
+
+namespace ProjetoElevador
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string acaoEscolhida; 
+            bool continuar = true; 
+
+            Elevador elevador = new Elevador(); 
+
+            elevador.Inicio(); 
+
+            do
+            {
+                Console.WriteLine(" Escolha uma opção: ");
+                Console.WriteLine(" 1 - Entrar ");
+                Console.WriteLine(" 2 - Subir ");
+                Console.WriteLine(" 3 - Descer ");
+                Console.WriteLine(" 4 - Sair ");
+                Console.WriteLine(" 5 - Sair App ");
+    
+
+                acaoEscolhida = Console.ReadLine();
+                Console.Clear();
+
+                switch (acaoEscolhida) 
+                {
+                    case "1": Console.WriteLine("Entrar"); elevador.Entrar(); 
+                        break;
+                    case "2": Console.WriteLine("Subindo"); elevador.Subir(); 
+                        break;
+                    case "3": Console.WriteLine("Descendo"); elevador.Descer(); 
+                        break;
+                    case "4": Console.WriteLine("Sair"); elevador.Sair();
+                        break;
+                    case "5": Console.WriteLine("Sair App"); continuar = false; 
+                        break;
+                    default: Console.WriteLine("ERRO: opção inválida."); 
+                        break;
+                }
+                Console.WriteLine("");
+            }
+
+            while (continuar);
+
+
+        }
+
+    }
+}
+
+
+
+
